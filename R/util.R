8d970f08c76de5dc9a68a7077de610135d6b1bc0
Peter Carbonetto <peter.carbonetto@gmail.com>
2024-05-08 09:58:06 -0500

    Added a few more imports.

--- R/util.R
+++ R/util.R
@@ -1,4 +1,5 @@
-### initialize the EB-NMF fit to covariance matrix YY' s.t. E[YY'] = LL'+ D from an estimate of L without nonnegative constraints
+# Initialize the EB-NMF fit to covariance matrix YY' s.t. E[YY'] =
+# LL'+ D from an estimate of L without nonnegative constraints.
 init_cov_ebnmf <- function(fl, kset = 1:ncol(fl$flash_fit$EF[[1]])) {
   LL <- fl$flash_fit$EF[[1]][, kset, drop = FALSE]
   FF <- fl$flash_fit$EF[[2]][, kset, drop = FALSE]
@@ -6,7 +7,8 @@ init_cov_ebnmf <- function(fl, kset = 1:ncol(fl$flash_fit$EF[[1]])) {
   LL <- cbind(fl$flash_fit$EF[[1]][, -kset, drop = FALSE], LL)
   FF <- cbind(pmax(FF, 0), pmax(-FF, 0))
   FF <- cbind(fl$flash_fit$EF[[2]][, -kset, drop = FALSE], FF)
-  to.keep <- (colSums(LL) > .Machine$double.eps) & (colSums(FF) > .Machine$double.eps)
+  to.keep <- (colSums(LL) > .Machine$double.eps) &
+             (colSums(FF) > .Machine$double.eps)
   LL <- LL[, to.keep, drop = FALSE]
   FF <- FF[, to.keep, drop = FALSE]
   return(list(LL, FF))
@@ -17,6 +19,7 @@ init_cov_ebnmf <- function(fl, kset = 1:ncol(fl$flash_fit$EF[[1]])) {
 # s2*I and I is an identity matrix.
 #
 #' @importFrom Matrix rowSums
+#' @importFrom Matrix Diagonal
 fit_ebmf_to_YY <- function (dat, fl, extrapolate = TRUE, warmstart = TRUE,
                             maxiter = 500, tol = NULL, epsilon = 2e-2,
                             verbose = 1) {
@@ -42,7 +45,8 @@ fit_ebmf_to_YY <- function (dat, fl, extrapolate = TRUE, warmstart = TRUE,
     Y2_diff <- sum((data_diag - s2)^2 - (data_diag - old_s2)^2)
     fl <- fl %>%
       flash_update_data(dat_minuss2, Y2_diff = Y2_diff) %>%
-      flash_backfit(extrapolate = extrapolate, warmstart = warmstart, maxiter = maxiter, tol = tol, verbose = verbose)
+      flash_backfit(extrapolate = extrapolate, warmstart = warmstart,
+                    maxiter = maxiter, tol = tol, verbose = verbose)
     
     old_s2 <- s2
     s2 <- max(0, mean(data_diag - rowSums(fl$L_pm * fl$F_pm)))
@@ -58,24 +62,34 @@ fit_ebmf_to_YY <- function (dat, fl, extrapolate = TRUE, warmstart = TRUE,
 #' @importFrom stats cor
 #' @importFrom stats lm
 #' @importFrom stats aov
-fit_ebmf_to_Y <- function(Y, fit.cov, corr_thres, maxiter){
-  ### scale GEP membership estimates to 0-1 scale, and calculate Pearson correlations between L and L-tilde
+#' @importFrom Matrix crossprod
+#' @importFrom ebnm ebnm_generalized_binary
+#' @importFrom ebnm ebnm_point_laplace
+#' @importFrom ashr ash
+#' 
+fit_ebmf_to_Y <- function(Y, fit.cov, corr_thres, maxiter) {
+    
+  ### scale GEP membership estimates to 0-1 scale, and calculate
+  ### Pearson correlations between L and L-tilde
   k.order <- order(fit.cov$pve, decreasing = TRUE)
   fit.L <- fit.cov$L_pm[, k.order]
   fit.L <- t(t(fit.L)/apply(fit.L, 2, max))
   corr <- diag(cor(fit.cov$L_pm[, k.order], fit.cov$F_pm[, k.order]))
   
-  ### estimate GEP signatures by fitting EB-SNMF to gene expression data matrix with fixed L estimated from covariance decomposition above
+  ### estimate GEP signatures by fitting EB-SNMF to gene expression
+  ### data matrix with fixed L estimated from covariance decomposition
+  ### above
   init.F <- t(solve(crossprod(fit.L), as.matrix(Matrix::crossprod(fit.L, Y))))
   fit.snmf <- flash_init(Y, S = 1/sqrt(nrow(Y)), var_type = 2) %>%
     flash_factors_init(
       init = list(as.matrix(fit.L), as.matrix(init.F)),
-      ebnm_fn = c(ebnm::ebnm_generalized_binary, ebnm::ebnm_point_laplace)
+      ebnm_fn = c(ebnm_generalized_binary, ebnm_point_laplace)
     ) %>%
     flash_factors_fix(kset = 1:ncol(fit.L), which_dim = "loadings") %>%
     flash_backfit(extrapolate = FALSE, maxiter = maxiter, verbose = 1)
   
-  ### calculate the z-score and lfsr of GEP signatures by running linear regression followed by ash
+  ### calculate the z-score and lfsr of GEP signatures by running
+  ### linear regression followed by ash
   J <- ncol(Y)
   genes <- colnames(Y)
   F.est <- matrix(0, J, ncol(fit.snmf$L_pm))
@@ -95,12 +109,14 @@ fit_ebmf_to_Y <- function(Y, fit.cov, corr_thres, maxiter){
   }
   
   for(k in 1:ncol(F.est)){
-    fit <- ashr::ash(F.est[,k], F.se[,k], mixcompdist = "normal", method = "shrink")
+    fit <- ash(F.est[,k], F.se[,k], mixcompdist = "normal",method = "shrink")
     F.z.ash[,k] <- fit$result$PosteriorMean/fit$result$PosteriorSD
     F.lfsr.ash[,k] <- fit$result$lfsr
   }
   
-  ### return the estimated memberships and signatures only for GEPs whose L and Ltilde from covariance decomposition are highly concordant
+  ### return the estimated memberships and signatures only for GEPs
+  ### whose L and Ltilde from covariance decomposition are highly
+  ### concordant
   k.idx <- corr > corr_thres
   L.pm <- fit.snmf$L_pm[, k.idx]
   F.lfc <- fit.snmf$F_pm[, k.idx]/log(2)
