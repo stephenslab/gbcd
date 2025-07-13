670e3afb50236303ddf99db33a3394abd4a711bd
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-07-13 11:23:49 -0500

    Reverted some of the changes made in commit 7756240.

--- R/fit_gbcd.R
+++ R/fit_gbcd.R
@@ -44,11 +44,6 @@
 #' @param verbose Integer specifying whether and how to display
 #'   progress updates, as described in flashier.
 #'   
-#' @param ldf_type The type of diagonal scaling matrix D to use when
-#'   fitting the model \eqn{Y = LDF' + E}. Use \code{ldf_type = "i"}
-#'   to set D to the identity matrix; \code{ldf_type = "cov"} to set
-#'   D such that \eqn{LD^2L'} is approximately \eqn{YY'/p}.
-#'   
 #' @return A list including the following elements:
 #' 
 #' \item{L}{cell x GEP matrix containing the posterior estimates of
@@ -62,9 +57,6 @@
 #'   \code{log(2)}. For example, fitted values would be recovered as
 #'   \code{tcrossprod(L,log(2) * F$lfc)}.}
 #'   
-#' \item{D}{A vector containing the diagonal entries of the 
-#'   diagonal scaling matrix D.}
-#'
 #' @examples
 #' # Please see the vignettes for examples.
 #'
@@ -83,9 +75,7 @@
 #' 
 fit_gbcd <- function (Y, Kmax, prior = ebnm::ebnm_generalized_binary, 
                       maxiter1 = 500, maxiter2 = 200, maxiter3 = 500,
-                      control = list(), verbose = 1, 
-                      ldf_type = c("i","cov")) {
-  ldf_type <- match.arg(ldf_type)
+                      control = list(), verbose = 1) {
   control <- modifyList(fit_gbcd_control_default(), control, 
                         keep.null = TRUE)
   extrapolate <- control$extrapolate
@@ -156,7 +146,7 @@ fit_gbcd <- function (Y, Kmax, prior = ebnm::ebnm_generalized_binary,
   ### above
   print("Estimate GEP signature matrix F...")
   start_time = proc.time()
-  res <- fit_ebmf_to_Y(Y, fit.cov, corr_thres, maxiter3, ldf_type)
+  res <- fit_ebmf_to_Y(Y, fit.cov, corr_thres, maxiter3)
   runtime = proc.time() - start_time
   print(runtime)
 
